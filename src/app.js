3eb1423593dcbc638705005e7d4e208705308e16
Edgar Diaz <ed35376@gmail.com>
2023-04-11 18:21:01 +0000

    First changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const who = ['The dog','My grandma','My cat','Mario Bros'];
+const action = ['ate','peed','crushed','broke', 'steal', 'picked'];
+const what = ['my homework', 'the keys', 'the car', 'the laptop'];
+const when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+
+onload =  () => {
+//write your code here
+let result = who[Math.floor(Math.random()*who.length)]+' '+action[Math.floor(Math.random()*action.length)]+' '+what[Math.floor(Math.random()*what.length)]+' '+when[Math.floor(Math.random()*when.length)];
+document.querySelector('#excuse').innerHTML= result
+// return result
+}    
+
+
