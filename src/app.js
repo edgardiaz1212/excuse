53f17dd66281d0878961e4e2fb43d992320ffc39
Edgar Diaz <ed35376@gmail.com>
2023-04-11 18:24:59 +0000

    fixing the format

--- src/app.js
+++ src/app.js
@@ -5,17 +5,27 @@
 // import "./assets/img/rigo-baby.jpg";
 // import "./assets/img/4geeks.ico";
 
-const who = ['The dog','My grandma','My cat','Mario Bros'];
-const action = ['ate','peed','crushed','broke', 'steal', 'picked'];
-const what = ['my homework', 'the keys', 'the car', 'the laptop'];
-const when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+const who = ["The dog", "My grandma", "My cat", "Mario Bros"];
+const action = ["ate", "peed", "crushed", "broke", "steal", "picked"];
+const what = ["my homework", "the keys", "the car", "the laptop"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying",
+];
 
-
-onload =  () => {
-//write your code here
-let result = who[Math.floor(Math.random()*who.length)]+' '+action[Math.floor(Math.random()*action.length)]+' '+what[Math.floor(Math.random()*what.length)]+' '+when[Math.floor(Math.random()*when.length)];
-document.querySelector('#excuse').innerHTML= result
-// return result
-}    
-
-
+onload = () => {
+  //write your code here
+  let result =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+  document.querySelector("#excuse").innerHTML = result;
+  return result
+};
